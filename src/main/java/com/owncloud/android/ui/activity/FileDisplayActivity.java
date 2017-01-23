ee3f10c83be64474280065b3fda5711adbd667a7
Mario Danic <mario@lovelyhq.com>
2017-01-23 10:34:08 +0100

    Fix issues brought up by review

--- src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -140,6 +140,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import static com.owncloud.android.R.drawable.file;
+import static com.owncloud.android.db.PreferenceManager.getSortAscending;
 import static com.owncloud.android.db.PreferenceManager.getSortOrder;
 
 /**
@@ -203,7 +205,7 @@ public class FileDisplayActivity extends HookActivity
     public static final String KEY_IS_SEARCH_OPEN = "IS_SEARCH_OPEN";
     public static final String KEY_SEARCH_QUERY = "SEARCH_QUERY";
 
-    private String mSearchQuery;
+    private String mSearchQuery = "";
     private boolean mSearchOpen;
 
     private SearchView mSearchView;
@@ -1103,9 +1105,7 @@ public class FileDisplayActivity extends HookActivity
         // mRefreshSharesInProgress);
         outState.putParcelable(FileDisplayActivity.KEY_WAITING_TO_SEND, mWaitingToSend);
         outState.putBoolean(KEY_IS_SEARCH_OPEN, !mSearchView.isIconified());
-        if (mSearchQuery != null) {
-            outState.putString(KEY_SEARCH_QUERY, mSearchQuery);
-        }
+        outState.putString(KEY_SEARCH_QUERY, mSearchQuery);
 
         Log_OC.v(TAG, "onSaveInstanceState() end");
     }
@@ -2269,9 +2269,7 @@ public class FileDisplayActivity extends HookActivity
             args.putParcelable(EXTRA_FILE, file);
             args.putParcelable(EXTRA_ACCOUNT, getAccount());
             args.putBoolean(EXTRA_SEARCH, mSearchOpen);
-            if (mSearchQuery != null) {
-                args.putString(EXTRA_SEARCH_QUERY, mSearchQuery);
-            }
+            args.putString(EXTRA_SEARCH_QUERY, mSearchQuery)
             Fragment textPreviewFragment = Fragment.instantiate(getApplicationContext(),
                     PreviewTextFragment.class.getName(), args);
             setSecondFragment(textPreviewFragment);
